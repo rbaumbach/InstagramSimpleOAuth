1db4b5fd639a34ce80a9e3b653f07680ca0cd74a
rbaumbach <rbaumbach.github@gmail.com>
2014-12-13 12:05:20 -0800

    Call completion block after view controller dismissal instead of before

--- InstagramSimpleOAuth/Source/InstagramSimpleOAuthViewController.m
+++ InstagramSimpleOAuth/Source/InstagramSimpleOAuthViewController.m
@@ -140,22 +140,22 @@ NSString *const InstagramLoginCancelButtonTitle = @"OK";
 
 - (void)completeAuthWithLoginResponse:(InstagramLoginResponse *)response
 {
-    self.completion(response, nil);
-    
     [self dismissViewController];
     [self hideProgressHUD];
+    
+    self.completion(response, nil);
 }
 
 - (void)completeWithError:(NSError *)error
 {
-    self.completion(nil, error);
+    [self dismissViewController];
+    [self hideProgressHUD];
     
     if (self.shouldShowErrorAlert) {
         [self showErrorAlert:error];
     }
     
-    [self dismissViewController];
-    [self hideProgressHUD];
+    self.completion(nil, error);
 }
 
 - (void)dismissViewController
